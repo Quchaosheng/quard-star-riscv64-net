0b4af939f6ef13f0d3f6ff4e441d7e6f40ac6994
Quchaosheng <quchaosheng000406@163.com>
2026-07-19 15:29:25 +0800

    test: expect continuous timer reload catchup

--- tests/host/test_m6_timer.c
+++ tests/host/test_m6_timer.c
@@ -90,7 +90,7 @@ static void test_reload_uses_next_check_baseline(void)
     assert(slow_state.calls == 1);
 
     assert(net_timer_check_tmo(10) == NET_ERR_OK);
-    assert(fast_state.calls == 3);
+    assert(fast_state.calls == 4);
     assert(net_timer_first_tmo() == 10);
     net_timer_remove(&fast);
 }
