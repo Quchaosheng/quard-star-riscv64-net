790277871d284d49e57350aa29a8d9e8423c9b71
Quchaosheng <quchaosheng000406@163.com>
2026-07-22 12:02:32 +0800

    fix: reclaim file handles and retry tftp

--- kernel/include/timeros/file.h
+++ kernel/include/timeros/file.h
@@ -5,6 +5,7 @@
 #include <timeros/net/net_err.h>
 
 void file_init(void);
+void file_close_owner(int pid);
 int file_open(const char *name, int writable);
 int file_read(int handle, void *data, size_t length);
 int file_write(int handle, const void *data, size_t length);
